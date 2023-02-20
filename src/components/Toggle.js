ae5ff5454ed0faf401cd42473be377d4d1360286
NabiL Ashbat <nbl.dev21@gmail.com>
2023-02-20 10:24:54 +0300

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+    const [onOFToggle, setOnOFToggle] = useState(false);
+    const buttonRef = useRef(null);
+
+    const handleOnOFToggle = () => {
+        const newOnOFToggle = !onOFToggle;
+        setOnOFToggle(newOnOFToggle);
+        // onToggleClick(newOnOFToggle, buttonRef.current.innerText);
+    };
+
+    const color = onOFToggle?"red":"white"
+    return (
+        <button style={{backgroundColor: color}} ref={buttonRef} onClick={handleOnOFToggle}>
+            {onOFToggle ? "ON" : "OFF"}
+        </button>
+    );
 }
 
 export default Toggle;
